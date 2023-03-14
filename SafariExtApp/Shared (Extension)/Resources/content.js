29e762f28db93dd744b987ad37e9a27613a4be01
Vikash Rai <vikash27hbk@gmail.com>
2023-03-14 19:10:43 +0530

    ..

--- SafariExtApp/Shared (Extension)/Resources/content.js
+++ SafariExtApp/Shared (Extension)/Resources/content.js
@@ -39,23 +39,6 @@ function getMachineId() {
         machineId = crypto.randomUUID();
         localStorage.setItem('MachineId', machineId);
     }
-    
-//    var browserInfo = navigator.userAgent + navigator.language + (new Date()).getTime();
-//    var uniqueId = btoa(browserInfo);
-//    console.log('My unique identifier is: ' + uniqueId);
-//
-//
-//
-//    var uniqueId = localStorage.getItem('myUniqueId');
-//
-//    // If the unique identifier is not found, generate a new one and store it in local storage
-//    if (!uniqueId) {
-//        uniqueId = Math.random().toString(36).substr(2, 9);
-//        localStorage.setItem('myUniqueId', uniqueId);
-//    }
-//    console.log('My unique identifier is: ' + uniqueId);
-    
-
     return machineId;
 }
 
