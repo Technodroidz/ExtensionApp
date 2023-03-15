915926e7c8c12283bb1539a84da7a4a5de077cc1
Vikash Rai <vikash27hbk@gmail.com>
2023-03-15 19:34:01 +0530

    ..
    ..

---
+++ SafariExtApp/ExtensionMobileApp/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  ExtensionMobileApp
+//
+//  Created by Gaurav Tomar on 15/03/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
