da4360c42a19575d86bb421444c16216b57021e5
Vikash Rai <vikash27hbk@gmail.com>
2023-03-16 18:34:48 +0530

    1.0.2
    1.0.2

--- WebAndApiExt/routes/web.php
+++ WebAndApiExt/routes/web.php
@@ -14,18 +14,18 @@ use App\Http\Controllers\MainController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Route::get('/cleareverything', function () {
-//     $clearcache = Artisan::call('cache:clear');
-//     echo "Cache cleared<br>";
+Route::get('/cleareverything', function () {
+    $clearcache = Artisan::call('cache:clear');
+    echo "Cache cleared<br>";
 
-//     $clearview = Artisan::call('view:clear');
-//     echo "View cleared<br>";
+    $clearview = Artisan::call('view:clear');
+    echo "View cleared<br>";
 
-//     $clearconfig = Artisan::call('config:cache');
-//     echo "Config cleared<br>";
+    $clearconfig = Artisan::call('config:cache');
+    echo "Config cleared<br>";
 
-//     $cleardebugbar = Artisan::call('debugbar:clear');
-//     echo "Debug Bar cleared<br>";
-// });
+    $cleardebugbar = Artisan::call('debugbar:clear');
+    echo "Debug Bar cleared<br>";
+});
 
 Route::get('/index',[MainController::class, 'index']);
