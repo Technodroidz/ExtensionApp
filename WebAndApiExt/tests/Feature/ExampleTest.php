9ff439d50b894d74b2e105df131453a2e91c9867
Vikash Rai <vikash27hbk@gmail.com>
2023-03-24 17:02:51 +0530

    ..
    ..

--- WebAndApiExt/tests/Feature/ExampleTest.php
+++ WebAndApiExt/tests/Feature/ExampleTest.php
@@ -5,7 +5,6 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use RefreshDatabase;
 
     /**
      * A basic test example.
@@ -19,6 +18,45 @@ class ExampleTest extends TestCase
         $response->assertStatus(404);
     }
 
+       /**
+     * A basic load test example for performance and scalability.
+     *
+     * @return void
+     */
+    public function testLoad()
+    {
+        $totalRequests = 100;
+        $concurrency = 10;
+
+        $this->artisan('route:cache');
+        $this->artisan('config:cache');
+
+        $this->get('/');
+
+        $this->benchmark(function () use ($totalRequests, $concurrency) {
+            $this->get('/non-existing-url')->assertStatus(404);
+        }, $totalRequests, $concurrency);
+    }
+
+    /**
+     * Helper method to run a function multiple times and measure the time taken.
+     *
+     * @param callable $function The function to run.
+     * @param int $times The number of times to run the function.
+     * @return array An array containing the times taken for each run.
+     */
+    private function benchmark(callable $function, $times)
+    {
+        $timesTaken = [];
+
+        for ($i = 0; $i < $times; $i++) {
+            $startTime = microtime(true);
+            $function();
+            $timesTaken[] = microtime(true) - $startTime;
+        }
+
+        return $timesTaken;
+    }
  
 }
 
