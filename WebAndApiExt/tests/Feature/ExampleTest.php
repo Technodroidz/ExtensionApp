a8711f4bc2c9d1bbaa651318842e0a62509b707a
Vikash Rai <vikash27hbk@gmail.com>
2023-03-24 16:56:49 +0530

    Add Test Case
    Added test cases for laravel app

--- WebAndApiExt/tests/Feature/ExampleTest.php
+++ WebAndApiExt/tests/Feature/ExampleTest.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Tests\Feature;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -18,4 +18,46 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(404);
     }
+
+    /**
+     * A basic load test example for performance and scalability.
+     *
+     * @return void
+     */
+    public function testLoad()
+    {
+        $totalRequests = 100;
+        $concurrency = 10;
+
+        $this->artisan('route:cache');
+        $this->artisan('config:cache');
+
+        $this->get('/');
+
+        $this->benchmark(function () use ($totalRequests, $concurrency) {
+            $this->get('/non-existing-url')->assertStatus(404);
+        }, $totalRequests, $concurrency);
+    }
+
+    /**
+     * Helper method to run a function multiple times and measure the time taken.
+     *
+     * @param callable $function The function to run.
+     * @param int $times The number of times to run the function.
+     * @return array An array containing the times taken for each run.
+     */
+    private function benchmark(callable $function, $times)
+    {
+        $timesTaken = [];
+
+        for ($i = 0; $i < $times; $i++) {
+            $startTime = microtime(true);
+            $function();
+            $timesTaken[] = microtime(true) - $startTime;
+        }
+
+        return $timesTaken;
+    }
 }
+
+
