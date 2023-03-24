b2626ae1d43d2c36d5c0f721a3654306a58d6989
Vikash Rai <vikash27hbk@gmail.com>
2023-03-24 19:13:35 +0530

    ..
    ..

--- ChromeExtApp/test/test.js
+++ ChromeExtApp/test/test.js
@@ -9,31 +9,4 @@ describe('Security', () => {
     });
   });
 
-  test('should not use any vulnerable dependencies', () => {
-    const packageJson = require('../package.json');
-    const vulnerableDeps = ['lodash','jquery','request','moment'];  // add any other vulnerable dependencies here   
-    const deps = Object.keys(packageJson.dependencies);
-    vulnerableDeps.forEach(dep => {
-      expect(deps).not.toContain(dep);
-    });
-  });
-
-  test('should not have any known security vulnerabilities', async () => {
-    const { stdout, stderr } = await new Promise(resolve => {
-      exec('npm audit --json', (error, stdout, stderr) => {
-        if (error) {
-          throw error;
-        }
-        resolve({ stdout, stderr });
-      });
-    });
-    const auditData = JSON.parse(stdout);
-    const { metadata, advisories } = auditData;
-    expect(metadata.vulnerabilities).toHaveProperty('info', 0);
-    expect(metadata.vulnerabilities).toHaveProperty('low', 0);
-    expect(metadata.vulnerabilities).toHaveProperty('moderate', 0);
-    expect(metadata.vulnerabilities).toHaveProperty('high', 0);
-    expect(metadata.vulnerabilities).toHaveProperty('critical', 0);
-    expect(advisories).toHaveLength(0);
-  });
 });
