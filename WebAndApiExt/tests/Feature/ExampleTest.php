a9b9d2053455e394c74e753f30b59eab20f20e6a
Vikash Rai <vikash27hbk@gmail.com>
2023-03-24 17:10:19 +0530

    ..
    ..

--- WebAndApiExt/tests/Feature/ExampleTest.php
+++ WebAndApiExt/tests/Feature/ExampleTest.php
@@ -1,24 +1,28 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
 
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_url()
     {
         $response = $this->get('/non-existing-url');
 
         $response->assertStatus(404);
     }
 
-       /**
+    /**
      * A basic load test example for performance and scalability.
      *
      * @return void
@@ -39,25 +43,46 @@ class ExampleTest extends TestCase
     }
 
     /**
+     * A security test example for vulnerabilities.
+     *
+     * @return void
+     */
+    public function testSecurity()
+    {
+        // Test for potential SQL injection vulnerability in user input
+        $response = $this->get('/search?q=\'; DROP TABLE users;--');
+        $response->assertStatus(500);
+
+        // Test for potential XSS vulnerability in user input
+        $response = $this->get('/search?q=<script>alert("Hello world!");</script>');
+        $response->assertDontSee('<script>');
+    }
+
+    /**
      * Helper method to run a function multiple times and measure the time taken.
      *
      * @param callable $function The function to run.
      * @param int $times The number of times to run the function.
+     * @param int $concurrency The number of requests to run concurrently.
      * @return array An array containing the times taken for each run.
      */
-    private function benchmark(callable $function, $times)
+    private function benchmark(callable $function, $times, $concurrency = 1)
     {
         $timesTaken = [];
 
-        for ($i = 0; $i < $times; $i++) {
-            $startTime = microtime(true);
-            $function();
-            $timesTaken[] = microtime(true) - $startTime;
+        for ($i = 0; $i < $times; $i += $concurrency) {
+            $responses = [];
+
+            for ($j = 0; $j < $concurrency; $j++) {
+                $responses[] = $function();
+            }
+
+            $timesTaken[] = array_sum($responses);
         }
 
         return $timesTaken;
     }
- 
 }
+
 
 
