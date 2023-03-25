a56a090cbebb899c406269995e16e514787849fd
Vikash Rai <vikash27hbk@gmail.com>
2023-03-25 20:55:12 +0530

    ..
    ..

--- SafariExtApp/test/test.js
+++ SafariExtApp/test/test.js
@@ -1,12 +1,16 @@
 const assert = require('assert');
 const jsdom = require('jsdom-global');
 
+// Call jsdom-global before running tests to mock the Safari environment
 jsdom();
 
 describe('Security', function() {
     test('should not use any vulnerable dependencies', () => {
-      const manifest = safari.extension.baseURI + 'manifest.json';
-      const content = safari.extension.secureSettings.content;
+      const manifest = './package.json'; // Update with the path to your manifest file
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', manifest, false);
+      xhr.send(null);
+      const content = JSON.parse(xhr.responseText);
   
       retire.scanNode(content, function (result) {
         expect(result.length).toBe(0);
