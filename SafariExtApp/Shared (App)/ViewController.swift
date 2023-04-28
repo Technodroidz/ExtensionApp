75c89782a667324e9ea64b811094556ef497eb02
Technodroidz <79449782+Technodroidz@users.noreply.github.com>
2023-04-28 15:49:46 +0530

    ..
    ..

--- SafariExtApp/Shared (App)/ViewController.swift
+++ SafariExtApp/Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "com.safaricom.extApp.Extension"
+let extensionBundleIdentifier = "com.safaricomt.extApp.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
