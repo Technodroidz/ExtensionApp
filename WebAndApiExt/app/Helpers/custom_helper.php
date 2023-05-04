3ac53619aa513f6c62ae99d0106d62d935d9ae78
Vikash Rai <vikash27hbk@gmail.com>
2023-05-04 21:58:08 +0530

    ..
    ..

--- WebAndApiExt/app/Helpers/custom_helper.php
+++ WebAndApiExt/app/Helpers/custom_helper.php
@@ -1,5 +1,15 @@
 <?php
     
+    function getRandomString($length) {
+        $characters = '0123456789';
+        $string = '';
+ 
+        for ($i = 0; $i < $length; $i++) {
+          $string .= $characters[mt_rand(0, strlen($characters) - 1)];
+      }
+ 
+       return $string;
+    }
 
     function pp($arr, $die="true")
     {
